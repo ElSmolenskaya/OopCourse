08a98e4a3d2a3a0b75430bec84cb188ba681f3b3
ElSmolenskaya <el.smolenskaya@icloud.com>
2022-07-29 20:01:28 +0700

    Add Range project

--- Range/src/ru/academits/smolenskaya/range/Range.java
+++ Range/src/ru/academits/smolenskaya/range/Range.java
@@ -1,4 +1,73 @@
 package ru.academits.smolenskaya.range;
 
 public class Range {
+    private double from;
+    private double to;
+
+    public Range(double from, double to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public double getFrom() {
+        return from;
+    }
+
+    public void setFrom(double from) {
+        this.from = from;
+    }
+
+    public double getTo() {
+        return to;
+    }
+
+    public void setTo(double to) {
+        this.to = to;
+    }
+
+    public double getLength() {
+        return to - from;
+    }
+
+    public boolean isInside(double number) {
+        double epsilon = 1.0e-10;
+
+        return number - from >= -epsilon && to - number >= -epsilon;
+    }
+
+    public Range intersection(Range range) {
+        if (range.getTo() <= from || range.getFrom() >= to) {
+            return null;
+        }
+
+        return new Range(Math.max(from, range.getFrom()), Math.min(to, range.getTo()));
+    }
+
+    public Range[] union(Range range) {
+        if (range.getTo() < from || range.getFrom() > to) {
+            return new Range[]{new Range(from, to), new Range(range.getFrom(), range.getTo())};
+        }
+
+        return new Range[]{new Range(Math.min(from, range.getFrom()), Math.max(to, range.getTo()))};
+    }
+
+    public Range[] difference(Range range) {
+        if (to <= range.getFrom() || from >= range.getTo()) {
+            return new Range[]{new Range(from, to)};
+        }
+
+        if (from < range.getFrom()) {
+            if (to <= range.getTo()) {
+                return new Range[]{new Range(from, range.getFrom())};
+            }
+
+            return new Range[]{new Range(from, range.getFrom()), new Range(range.getTo(), to)};
+        }
+
+        if (to > range.getTo()) {
+            return new Range[]{new Range(range.getTo(), to)};
+        }
+
+        return null;
+    }
 }
--- Range/src/ru/academits/smolenskaya/range_main/Main.java
+++ Range/src/ru/academits/smolenskaya/range_main/Main.java
@@ -1,7 +1,30 @@
 package ru.academits.smolenskaya.range_main;
 
+import ru.academits.smolenskaya.range.Range;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello world!");
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Range lineSegment = new Range(0, 0);
+
+        System.out.print("Введите начальное число: ");
+        lineSegment.setFrom(scanner.nextDouble());
+
+        System.out.print("Введите конечное число: ");
+        lineSegment.setTo(scanner.nextDouble());
+
+        System.out.printf("Задан отрезок прямой от %fсм. до %fсм.%n", lineSegment.getFrom(), lineSegment.getTo());
+        System.out.printf("Длина отрезка равна %fсм.%n", lineSegment.getLength());
+
+        System.out.print("Введите число: ");
+        double userNumber = scanner.nextDouble();
+
+        if (lineSegment.isInside(userNumber)) {
+            System.out.print("Число принадлежит заданному отрезку");
+        } else {
+            System.out.print("Число не принадлежит заданному отрезку");
+        }
     }
 }
