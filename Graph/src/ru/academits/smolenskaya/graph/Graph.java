fb57d3af129670c7a7368ed43554ddabb955bdf1
ElSmolenskaya <el.smolenskaya@icloud.com>
2022-09-15 10:39:32 +0700

    Исправлены счетчики в модуле Graph

--- Graph/src/ru/academits/smolenskaya/graph/Graph.java
+++ Graph/src/ru/academits/smolenskaya/graph/Graph.java
@@ -29,39 +29,26 @@ public class Graph {
         }
     }
 
-    @SuppressWarnings("unused")
-    public void removeEdge(int node1, int node2) {
-        if (node1 < 0 || node1 >= edges.length) {
-            throw new IllegalArgumentException("Node1 = " + node1 + ": node1 must be >= 0 and < " + edges.length);
-        }
-
-        if (node2 < 0 || node2 >= edges.length) {
-            throw new IllegalArgumentException("Node2 = " + node2 + ": node2 must be >= 0 and < " + edges.length);
-        }
-
-        edges[node1][node2] = 0;
-        edges[node2][node1] = 0;
-    }
-
     public void widthTraverse(Consumer<Integer> consumer) {
         boolean[] visited = new boolean[edges.length];
 
-        for (int unvisitedNode = 0; unvisitedNode < visited.length; unvisitedNode++) {
-            if (!visited[unvisitedNode]) {
+        for (int i = 0; i < visited.length; i++) {
+            if (!visited[i]) {
                 LinkedList<Integer> queue = new LinkedList<>();
-                queue.add(unvisitedNode);
+
+                queue.add(i);
 
                 while (queue.size() > 0) {
-                    Integer node = queue.remove(0);
+                    int j = queue.remove(0);
 
-                    if (!visited[node]) {
-                        consumer.accept(node);
+                    if (!visited[j]) {
+                        consumer.accept(j);
 
-                        visited[node] = true;
+                        visited[j] = true;
 
-                        for (int relatedNode = node + 1; relatedNode < edges.length; relatedNode++) {
-                            if (edges[node][relatedNode] != 0 && !visited[relatedNode]) {
-                                queue.add(relatedNode);
+                        for (int k = j + 1; k < edges.length; k++) {
+                            if (edges[j][k] != 0 && !visited[k]) {
+                                queue.add(k);
                             }
                         }
                     }
@@ -73,23 +60,23 @@ public class Graph {
     public void depthTraverse(Consumer<Integer> consumer) {
         boolean[] visited = new boolean[edges.length];
 
-        for (int unvisitedNode = 0; unvisitedNode < visited.length; unvisitedNode++) {
-            if (!visited[unvisitedNode]) {
+        for (int i = 0; i < visited.length; i++) {
+            if (!visited[i]) {
                 LinkedList<Integer> stack = new LinkedList<>();
 
-                stack.add(unvisitedNode);
+                stack.add(i);
 
                 while (stack.size() > 0) {
-                    Integer node = stack.remove(stack.size() - 1);
+                    int j = stack.remove(stack.size() - 1);
 
-                    if (!visited[node]) {
-                        consumer.accept(node);
+                    if (!visited[j]) {
+                        consumer.accept(j);
 
-                        visited[node] = true;
+                        visited[j] = true;
 
-                        for (int relatedNode = node + 1; relatedNode < edges.length; relatedNode++) {
-                            if (edges[node][relatedNode] != 0 && !visited[relatedNode]) {
-                                stack.add(relatedNode);
+                        for (int k = j + 1; k < edges.length; k++) {
+                            if (edges[j][k] != 0 && !visited[k]) {
+                                stack.add(k);
                             }
                         }
                     }
