0189de27a7e074145a399891ef2952e2c18cd3e2
ElSmolenskaya <el.smolenskaya@icloud.com>
2022-10-04 18:31:46 +0700

    Исправлены ошибки в модуле Graph

--- Graph/src/ru/academits/smolenskaya/graph/Graph.java
+++ Graph/src/ru/academits/smolenskaya/graph/Graph.java
@@ -6,7 +6,6 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.function.Consumer;
 
-@SuppressWarnings("ClassCanBeRecord")
 public class Graph {
     private final double[][] edges;
 
@@ -15,8 +14,11 @@ public class Graph {
             throw new IllegalArgumentException("Edges.length = " + edges.length + ": nodes count must be > 0");
         }
 
-        if (edges[0].length == 0) {
-            throw new IllegalArgumentException("Edges[0].length = " + edges[0].length + ": nodes count must be > 0");
+        for (int i = 0; i < edges.length; i++) {
+            if (edges[i].length != edges.length) {
+                throw new IllegalArgumentException("Edges[" + i + "].length = " + edges[i].length + " is not equal edges.length = " +
+                        edges.length + ": matrix must be square and all rows must be of the same length");
+            }
         }
 
         this.edges = new double[edges.length][edges[0].length];
@@ -27,17 +29,26 @@ public class Graph {
     }
 
     @SuppressWarnings("unused")
-    public void setEdge(int nodeIndex1, int nodeIndex2, double edgeWeight) {
-        if (nodeIndex1 < 0 || nodeIndex1 >= edges.length) {
-            throw new IllegalArgumentException("NodeIndex1 = " + nodeIndex1 + ": nodeIndex1 must be >= 0 and < " + edges.length);
+    public Graph(int size) {
+        if (size <= 0) {
+            throw new IllegalArgumentException("Size = " + size + ": size must be > 0");
         }
 
-        if (nodeIndex2 < 0 || nodeIndex2 >= edges.length) {
-            throw new IllegalArgumentException("NodeIndex2 = " + nodeIndex2 + ": nodeIndex2 must be >= 0 and < " + edges.length);
+        this.edges = new double[size][size];
+    }
+
+    @SuppressWarnings("unused")
+    public void setEdge(int node1Index, int node2Index, double edgeWeight) {
+        if (node1Index < 0 || node1Index >= edges.length) {
+            throw new IndexOutOfBoundsException("Node1Index = " + node1Index + ": node1Index must be >= 0 and < " + edges.length);
         }
 
-        if (nodeIndex1 != nodeIndex2) {
-            edges[nodeIndex1][nodeIndex2] = edgeWeight;
+        if (node2Index < 0 || node2Index >= edges.length) {
+            throw new IndexOutOfBoundsException("Node2Index = " + node2Index + ": node2Index must be >= 0 and < " + edges.length);
+        }
+
+        if (node1Index != node2Index) {
+            edges[node1Index][node2Index] = edgeWeight;
         }
     }
 
@@ -51,17 +62,15 @@ public class Graph {
                 queue.add(i);
 
                 while (!queue.isEmpty()) {
-                    int nodeIndex1 = queue.remove();
+                    int node1Index = queue.remove();
 
-                    if (!visited[nodeIndex1]) {
-                        consumer.accept(nodeIndex1);
+                    consumer.accept(node1Index);
 
-                        visited[nodeIndex1] = true;
+                    for (int node2Index = 0; node2Index < edges.length; node2Index++) {
+                        if (edges[node1Index][node2Index] != 0 && !visited[node2Index]) {
+                            queue.add(node2Index);
 
-                        for (int nodeIndex2 = 0; nodeIndex2 < edges.length; nodeIndex2++) {
-                            if (edges[nodeIndex1][nodeIndex2] != 0 && !visited[nodeIndex2]) {
-                                queue.add(nodeIndex2);
-                            }
+                            visited[node2Index] = true;
                         }
                     }
                 }
@@ -79,17 +88,15 @@ public class Graph {
                 stack.addLast(i);
 
                 while (!stack.isEmpty()) {
-                    int nodeIndex1 = stack.removeLast();
+                    int node1Index = stack.removeLast();
 
-                    if (!visited[nodeIndex1]) {
-                        consumer.accept(nodeIndex1);
+                    consumer.accept(node1Index);
 
-                        visited[nodeIndex1] = true;
+                    for (int node2Index = edges.length - 1; node2Index >= 0; node2Index--) {
+                        if (edges[node1Index][node2Index] != 0 && !visited[node2Index]) {
+                            stack.addLast(node2Index);
 
-                        for (int nodeIndex2 = edges.length - 1; nodeIndex2 >= 0; nodeIndex2--) {
-                            if (edges[nodeIndex1][nodeIndex2] != 0 && !visited[nodeIndex2]) {
-                                stack.addLast(nodeIndex2);
-                            }
+                            visited[node2Index] = true;
                         }
                     }
                 }
--- Temperature/src/ru/academits/smolenskaya/temperature_view/Main.java
+++ Temperature/src/ru/academits/smolenskaya/temperature_view/Main.java
@@ -1,39 +1,83 @@
 package ru.academits.smolenskaya.temperature_view;
 
 import javax.swing.*;
+import javax.swing.text.NumberFormatter;
 import java.awt.*;
+import java.text.NumberFormat;
 
 public class Main {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             JFrame temperatureFrame = new JFrame("Temperature conversion");
-            temperatureFrame.setSize(300, 200);
+            temperatureFrame.setSize(250, 110);
+            temperatureFrame.setResizable(false);
             temperatureFrame.setLocationRelativeTo(null);
             temperatureFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             temperatureFrame.setVisible(true);
 
-            JPanel temperaturePanel = new JPanel(new GridBagLayout());
-            temperatureFrame.add(temperaturePanel, BorderLayout.CENTER);
+            GridBagLayout temperatureGridBagLayout = new GridBagLayout();
+            temperatureFrame.setLayout(temperatureGridBagLayout);
 
-            GridBagConstraints c = new GridBagConstraints();
+            GridBagConstraints temperatureGridBagConstraints = new GridBagConstraints();
+            temperatureGridBagConstraints.anchor = GridBagConstraints.CENTER;
 
             String[] items = {
-                    "Элемент списка 1",
-                    "Элемент списка 2",
-                    "Элемент списка 3"
+                    "Celsius",
+                    "Kelvin",
+                    "Fahrenheit"
             };
-
+            //////////////////////////////////////////
             JComboBox temperatureFromComboBox = new JComboBox(items);
-            temperatureFromComboBox.setEditable(true);
             temperatureFromComboBox.setVisible(true);
-            temperaturePanel.add(temperatureFromComboBox, c);
+            temperatureFrame.add(temperatureFromComboBox);
 
+            temperatureGridBagConstraints.insets = new Insets(1, 1, 0, 1);
+            temperatureGridBagConstraints.gridx = 0;
+            temperatureGridBagConstraints.gridy = 0;
+            temperatureGridBagLayout.setConstraints(temperatureFromComboBox, temperatureGridBagConstraints);
+            //////////////////////////////////////////
+            NumberFormat temperatureNumberFormat = NumberFormat.getNumberInstance();
+            temperatureNumberFormat.setMaximumFractionDigits(4);
+
+            JFormattedTextField temperatureFromFormattedTextField = new JFormattedTextField(new NumberFormatter(temperatureNumberFormat));
+            temperatureFromFormattedTextField.setEditable(true);
+            temperatureGridBagConstraints.anchor = GridBagConstraints.CENTER;
+            temperatureFromFormattedTextField.setPreferredSize(new Dimension(120, temperatureFromComboBox.getPreferredSize().height));
+            temperatureFrame.add(temperatureFromFormattedTextField);
+
+            temperatureGridBagConstraints.gridx = 1;
+            temperatureGridBagConstraints.gridy = 0;
+            temperatureGridBagLayout.setConstraints(temperatureFromFormattedTextField, temperatureGridBagConstraints);
+            //////////////////////////////////////////
+            JButton conversingButton = new JButton("Convert");
+            conversingButton.setPreferredSize(new Dimension(237, 20));
+            temperatureFrame.add(conversingButton);
+
+            temperatureGridBagConstraints.gridwidth = 2;
+            temperatureGridBagConstraints.gridx = 0;
+            temperatureGridBagConstraints.gridy = 1;
+            temperatureGridBagLayout.setConstraints(conversingButton, temperatureGridBagConstraints);
+            /////////////////////////////////////////
             JComboBox temperatureToComboBox = new JComboBox(items);
-            temperatureToComboBox.setEditable(true);
             temperatureToComboBox.setVisible(true);
-            temperaturePanel.add(temperatureToComboBox, c);
+            temperatureFrame.add(temperatureToComboBox);
+
+            temperatureGridBagConstraints.gridx = 0;
+            temperatureGridBagConstraints.gridy = 2;
+            temperatureGridBagConstraints.insets = new Insets(3, 1, 0, 1);
+            temperatureGridBagConstraints.anchor = GridBagConstraints.NORTHWEST;
+            temperatureGridBagLayout.setConstraints(temperatureToComboBox, temperatureGridBagConstraints);
+
+            ////////////////
+            JFormattedTextField temperatureToFormattedTextField = new JFormattedTextField(new NumberFormatter(temperatureNumberFormat));
+            temperatureToFormattedTextField.setEditable(false);
+            temperatureGridBagConstraints.anchor = GridBagConstraints.CENTER;
+            temperatureToFormattedTextField.setPreferredSize(new Dimension(temperatureFromFormattedTextField.getPreferredSize().width, temperatureFromFormattedTextField.getPreferredSize().height));
+            temperatureFrame.add(temperatureToFormattedTextField);
+
+            temperatureGridBagConstraints.gridx = 1;
+            temperatureGridBagConstraints.gridy = 2;
+            temperatureGridBagLayout.setConstraints(temperatureToFormattedTextField, temperatureGridBagConstraints);
         });
-
-
     }
 }
